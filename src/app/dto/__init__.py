8573ff06beb1df7e8813dd33005dd93ad920c9c7
doworder <doworder@gmail.com>
2025-08-10 18:00:23 +0300

    add UpdateRateDTO

--- src/app/dto/__init__.py
+++ src/app/dto/__init__.py
@@ -5,10 +5,12 @@ __all__ = [
     "GetRateDTO",
     "QueryCurrencyDTO",
     "QueryRateDTO",
+    "UpdateRateDTO"
     "GetDTOFactory"
 ]
 
 from app.dto.factory import GetDTOFactory
+from app.dto.add import AddCurrencyDTO, AddRateDTO
 from app.dto.get import GetCurrencyDTO, GetRateDTO
 from app.dto.query import QueryCurrencyDTO, QueryRateDTO
-from app.dto.add import AddCurrencyDTO, AddRateDTO
+from app.dto.update import UpdateRateDTO
