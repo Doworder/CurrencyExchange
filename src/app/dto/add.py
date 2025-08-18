9230f68b4203773a0e4ef93a4c65469721420dd2
doworder <doworder@gmail.com>
2025-08-18 21:24:31 +0300

    add validation to the dataclasses

--- src/app/dto/add.py
+++ src/app/dto/add.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from decimal import Decimal
 
+from app.dto.validation_functions import (
+    is_valid_currency_code,
+    validate_currencies,
+    validate_exchange_rate
+)
+
 
 @dataclass
 class AddCurrencyDTO:
@@ -8,9 +14,16 @@ class AddCurrencyDTO:
     full_name: str
     sign: str
 
+    def __post_init__(self) -> None:
+        is_valid_currency_code(self.currency_code)
+
 
 @dataclass
 class AddRateDTO:
     base_currency: str
     target_currency: str
     rate: Decimal
+
+    def __post_init__(self) -> None:
+        validate_currencies(self.base_currency, self.target_currency)
+        validate_exchange_rate(self.rate)
